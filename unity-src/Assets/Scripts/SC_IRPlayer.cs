2832ade382d4e371fbfd07d28e476c7f61f1330e
seyiul <iseuli713@gmail.com>
2021-01-06 13:59:06 +0900

    capsule collision detection
    capsule collision detection

--- unity-src/Assets/Scripts/SC_IRPlayer.cs
+++ unity-src/Assets/Scripts/SC_IRPlayer.cs
@@ -1,20 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 [RequireComponent(typeof(Rigidbody))]
-
 public class SC_IRPlayer : MonoBehaviour
 {
     public float gravity = 20.0f;
     public float jumpHeight = 2.5f;
     public int life = 3;
-
     public Rigidbody r;
     bool grounded = false;
     Vector3 defaultScale;
     bool crouch = false;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,54 +20,43 @@ public class SC_IRPlayer : MonoBehaviour
         r.useGravity = false;
         defaultScale = transform.localScale;
     }
-
     void Update()
     {
-
         // Jump
         if (Input.GetKeyDown(KeyCode.W) && grounded)
         {
             r.velocity = new Vector3(r.velocity.x, CalculateJumpVerticalSpeed(), r.velocity.z);
         }
-
         //float h = Input.GetAxisRaw("Horizontal") * 2.25f;
-
         //transform.Translate(Vector3.right * h * Time.deltaTime * 1.0f);
         if (Input.GetKeyDown(KeyCode.A) && grounded)
         {
             if (transform.position.x != -2.25)
                 transform.Translate(new Vector3(-2.25f, r.velocity.y, r.velocity.z));
         }
-
         if (Input.GetKeyDown(KeyCode.D) && grounded)
         {
             if (transform.position.x != 2.25)
                 transform.Translate(new Vector3(2.25f, r.velocity.y, r.velocity.z));
         }
-
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         // We apply gravity manually for more tuning control
         r.AddForce(new Vector3(0, -gravity * r.mass, 0));
-
         grounded = false;
     }
-
     void OnCollisionStay()
     {
         grounded = true;
     }
-
     float CalculateJumpVerticalSpeed()
     {
         // From the jump height and gravity we deduce the upwards speed 
         // for the character to reach at the apex.
         return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Finish")
@@ -82,16 +67,21 @@ public class SC_IRPlayer : MonoBehaviour
                 life--;
                 SC_GroundGenerator.instance.gameOver = true;
             }
-            else
+            else if(life>1)
                 life--;
         }
-        if(collision.gameObject.tag == "life")
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "life")
         {
-            collision.gameObject.GetComponent<Renderer>().enabled = false;
             life++;
+            col.gameObject.GetComponent<Renderer>().enabled = false;
         }
     }
-
+    void OnTriggerExit(Collider col)
+    {
+    }
     void OnGUI()
     {
         GUI.Label(new Rect(5, 25, 200, 25), "Life: " + ((int)life));
