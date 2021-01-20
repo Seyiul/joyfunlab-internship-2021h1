1af54f905406be756df38009635c5d5c1bfeca92
seyiul <iseuli713@gmail.com>
2021-01-20 16:35:39 +0900

    BattleScene
    - Add highlight tiles

---
+++ unity-src/Assets/Battle/Scripts/PlayerAnim.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattlePlayerLocation : int
+{
+    Left = -1,
+    Center = 0,
+    Right = 1
+}
+public class PlayerAnim : MonoBehaviour
+{
+    public static PlayerAnim instance;
+
+    Animator animator;
+
+    public float hp;
+    public float monsterHp;
+
+    public float timer = 0;
+
+    public static bool attack;
+
+    public BattlePlayerLocation curLocation;
+    BattleHighlight highlightTiles;
+
+
+
+    void Awake() { instance = this; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        attack = false;
+        curLocation = BattlePlayerLocation.Center;
+        highlightTiles = GameObject.Find("HighlightTiles").GetComponent<BattleHighlight>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp = (float)PlayerHealthbarHandler.GetHealthBarValue() * 100;
+        monsterHp = (float)HealthBarHandler.GetHealthBarValue() * 100;
+        HandleGame(hp, monsterHp);
+
+    }
+    void HandleGame(float hp, float monsterHp)
+    {
+        if (monsterHp == 0)
+        {
+            animator.SetBool("victory", true);
+        }
+        else if (hp == 0)
+        {
+            animator.SetBool("defeat", true);
+
+        }
+        else
+            HandlePlayer();
+    }
+    void HandlePlayer()
+    {
+        HandlePlayerPosition();
+        HandlePlayerAction();
+
+        if (timer > 1f)
+        {
+            attack = false;
+        }
+
+
+    }
+    void HandlePlayerPosition()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            curLocation = BattlePlayerLocation.Left;
+            transform.position = new Vector3(101, transform.position.y, transform.position.z);
+            attack = false;
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            curLocation = BattlePlayerLocation.Center;
+            transform.position = new Vector3(114, transform.position.y, transform.position.z);
+            attack = false;
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            curLocation = BattlePlayerLocation.Right;
+            transform.position = new Vector3(127, transform.position.y, transform.position.z);
+            attack = false;
+
+        }
+        highlightTiles.Highlight(curLocation);
+    }
+    void HandlePlayerAction()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            animator.SetTrigger("kick");
+            attack = true;
+            timer = 0;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetTrigger("punch");
+            attack = true;
+            timer = 0;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            attack = false;
+            animator.SetTrigger("jump");
+        }
+
+    }
+    public static bool GetPlayerState()
+    {
+        if (attack == true)
+        {
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+
+}
---
+++ unity-src/Assets/Battle/Scripts/PlayerHealthbarHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthbarHandler : MonoBehaviour
+{
+    private static Image HealthBarImage;
+    private Text currentHp;
+
+    public static void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if (HealthBarImage.fillAmount < 0.3f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if (HealthBarImage.fillAmount < 0.6f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+
+    public static float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="healthColor">Color </param>
+    public static void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    private void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+        currentHp = GameObject.Find("PlayerCurrent").GetComponent<Text>();
+    }
+    private void Update()
+    {
+        var hp = (int)(HealthBarImage.fillAmount * 100);
+        currentHp.text = hp.ToString();
+    }
+}
