1de0f003e2a884a4c56b24392ec1d43e80d76438
Allannnn <nuts03@nate.com>
2021-01-22 15:13:27 +0900

    make game ui canvas

---
+++ unity-src/Assets/Scripts/UI/GameUICanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUICanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*
+    // 타일 실제 속도를 UI상의 속도로 변환 (30 ~ 90 -> 5km/s ~ 20km/s)
+    float ActualSpeedToDisplaySpeed(float actualSpeed)
+    {
+        return Mathf.Round((((actualSpeed - ConstInfo.initialActualSpeed) / 4);
+    }
+    */
+}
