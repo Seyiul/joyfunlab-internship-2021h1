da7b7e902658193736065bfd4117b0bb0fd9a4d4
Allannnn <nuts03@nate.com>
2021-01-22 15:20:33 +0900

    참고용 수식 업데이트

--- unity-src/Assets/Scripts/UI/GameUICanvas.cs
+++ unity-src/Assets/Scripts/UI/GameUICanvas.cs
@@ -17,9 +17,11 @@ public class GameUICanvas : MonoBehaviour
     }
     /*
     // 타일 실제 속도를 UI상의 속도로 변환 (30 ~ 90 -> 5km/s ~ 20km/s)
+    // actualSpeed = player.speed
+    // ConstInfo.initialActualSpeed = 30
     float ActualSpeedToDisplaySpeed(float actualSpeed)
     {
-        return Mathf.Round((((actualSpeed - ConstInfo.initialActualSpeed) / 4);
+       float ActualSpeedToDisplaySpeed(float actualSpeed) { return Mathf.Round((((actualSpeed - ConstInfo.initialActualSpeed) / 4) + 5) * 100) / 100; }
     }
     */
 }
