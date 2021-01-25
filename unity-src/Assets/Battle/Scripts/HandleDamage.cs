42b5a9abfedc9061ff970f0e9a1d8d4ee0ce530d
seyiul <iseuli713@gmail.com>
2021-01-25 17:47:50 +0900

    [Battle Scene] demaged effect Scene
    -

--- unity-src/Assets/Battle/Scripts/HandleDamage.cs
+++ unity-src/Assets/Battle/Scripts/HandleDamage.cs
@@ -97,7 +97,7 @@ public class HandleDamage : MonoBehaviour
     {
         blood.gameObject.SetActive(true);
         camera.GetComponent<ShakeBehavior>().TriggerShake();
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
         blood.gameObject.SetActive(false);
 
     }
