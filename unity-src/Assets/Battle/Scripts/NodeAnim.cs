427d0ff76644bd47ef5d5fb1a97daa52c94f1217
seyiul <iseuli713@gmail.com>
2021-01-28 16:12:19 +0900

    [Battle Scene] node time handler

---
+++ unity-src/Assets/Battle/Scripts/NodeAnim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeAnim : MonoBehaviour
+{
+    public GameObject leftKick;
+    public GameObject rightKick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartLeftKick());
+        StartCoroutine(StartRightKick());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator StartLeftKick()
+    {
+        yield return new WaitForSeconds(1f);
+        leftKick.GetComponent<Animator>().SetTrigger("start");
+
+    }
+    IEnumerator StartRightKick()
+    {
+        yield return new WaitForSeconds(1f);
+        rightKick.GetComponent<Animator>().SetTrigger("start");
+
+    }
+}
