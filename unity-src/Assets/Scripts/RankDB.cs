9caeb11987caf899bf0e705b7f78e755f57356cd
Allannnn <nuts03@nate.com>
2021-02-09 18:40:05 +0900

    rank update

--- unity-src/Assets/Scripts/RankDB.cs
+++ unity-src/Assets/Scripts/RankDB.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RankDB : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class RankDB {
+    public static void RankWriter(string rankpath,int[] rankArray)
     {
-        
+        StreamWriter sw = new StreamWriter(rankpath);
+        for (int i = 0; i < 5; i++) {
+            sw.WriteLine(rankArray[i]);
+        }
+        sw.Flush();
+        sw.Close();
     }
-
-    // Update is called once per frame
-    void Update()
+    public static int[] RankReader(string rankpath)
     {
-        
+        int[] rankArray = new int[5];
+        StreamReader sr = new StreamReader(rankpath);
+
+        for(int i = 0; i< 5; i++)
+        {
+            rankArray[i] = sr.ReadLine();
+        }
+        sr.Close();
+        return rankArray;
     }
 }
